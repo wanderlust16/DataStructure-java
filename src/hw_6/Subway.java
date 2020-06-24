be49c3b653e94bace347472b4372cc330edaabdb
jieun <wldms5764@gmail.com>
2020-06-24 11:24:31 +0900

    hw6 fin

--- src/hw_6/Subway.java
+++ src/hw_6/Subway.java
@@ -4,6 +4,10 @@ import java.io.*;
 import java.util.*;
 
 public class Subway {
+	private static final int TRANSFER_TIME = 5;
+	private static HashMap<String, Station> idMap = new HashMap<>();
+	private static HashMap<String, LinkedList<Station>> nameMap = new HashMap<>();
+
 	public static void main(String[] args) {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
@@ -12,9 +16,12 @@ public class Subway {
 			try
 			{
 				String input = br.readLine();
-				if (input.compareTo("QUIT") == 0)
-					break;
+				if(input.compareTo("QUIT") == 0) break;
 				if(input.startsWith("<")) enterInfo(input.substring(2));
+				else {
+					initNodes();
+					command(input);
+				}
 			}
 			catch (IOException e)
 			{
@@ -26,7 +33,6 @@ public class Subway {
 	public static void enterInfo(String fileName) throws IOException {
 		File file = new File(fileName);
 		String readMode = "stations";
-		Graph gr = new Graph();
 
 		if(file.exists()) {
 			BufferedReader br = new BufferedReader(new FileReader(file));
@@ -39,110 +45,129 @@ public class Subway {
 				} 
 				if(readMode == "stations") {
 					String[] splitInput = input.split("\\s+");
-					count++;
 						
 					String id = splitInput[0];
 					String name = splitInput[1];
 					String line = splitInput[2];	
-					Station st = new Station(id, name, line);
-					gr.put(st);
+					Station newSt = new Station(id, name, line);
 					
+					idMap.put(id, newSt);
+					LinkedList<Station> stations = nameMap.get(name);
+					
+					if(stations == null) {
+						stations = new LinkedList<>();
+						stations.add(newSt);
+						nameMap.put(name, stations);
+					} else {
+						for(Station st : stations) {
+							st.getEdges().add(new Edge(newSt, TRANSFER_TIME));
+							newSt.getEdges().add(new Edge(st, TRANSFER_TIME));
+						}
+						stations.add(newSt);
+					}
+										
 				} else {
 					if(input.isEmpty()) continue;
-					else {
-						String[] splitInput = input.split("\\s+");
+					
+					String[] splitInput = input.split("\\s+");
 	
-						String st1Id = splitInput[0];
-						String st2Id = splitInput[1];
-						int minutes = Integer.parseInt(splitInput[2]);
-						
-						Station st1 = gr.findById(st1Id);
-						Station st2 = gr.findById(st2Id);
-												
-						gr.addEdge(st1, st2, minutes);
-					}
+					String st1Id = splitInput[0];
+					String st2Id = splitInput[1];
+					long minutes = Long.parseLong(splitInput[2]);
+					
+					Station st1 = idMap.get(st1Id);
+					Station st2 = idMap.get(st2Id);
+					st1.getEdges().add(new Edge(st2, minutes));
 				}	
 			}
 			br.close();
 		}		
-		gr.print();
-	}
-}
-
-class Graph {
-	private int n; // node 개수 
-	private HashMap<Station, LinkedList<Edge>> hm;
-	
-//	public Graph(int n) {
-//		this.n = n;
-//	}
-	
-	public Graph() {
-		hm = new HashMap();
 	}
 	
-	public int getN() {
-		return n;
-	}
-
-	public void setN(int n) {
-		this.n = n;
-	}
-
-	public HashMap<Station, LinkedList<Edge>> getHm() {
-		return hm;
-	}
-
-	public void setHm(HashMap<Station, LinkedList<Edge>> hm) {
-		this.hm = hm;
-	}
-
-	public void put(Station st) {
-		LinkedList<Edge> llg = new LinkedList<Edge>();
-		hm.put(st, llg);
+	public static void command(String input) {
+		String[] splitInput = input.split("\\s+");
+		String srcName = splitInput[0];
+		String destName = splitInput[1];
+		dijkstra(srcName, destName);
 	}
 	
-	public void addEdge(Station st1, Station st2, int minutes) {
-		// hm은 이미 모든 station key를 당연히 가지고 있을 것 
-		// 양방향 연결은 이미 데이터에 개별 분리되어 있음 
-		LinkedList<Edge> llg = hm.get(st1);
-		Edge edge = new Edge(st1, st2, minutes);
-		llg.add(edge);
-	}
-	
-	public void dijkstra(Station source, Station dest) {
+	public static void dijkstra(String srcName, String destName) {
+		LinkedList<Station> srcStations = nameMap.get(srcName);
+		LinkedList<Station> destStations = nameMap.get(destName);
 		
-	}
-	
-	public Station findById(String Id) {
-		for(Station key : hm.keySet()) {
-			if(key.getId().equals(Id)) return key;
+		PriorityQueue<Station> unsettled = new PriorityQueue<>();
+		
+		for(Station st : srcStations) {
+			st.setMinutes(0);
 		}
-		return null;
+		unsettled.addAll(srcStations);
+		
+		int count = 1;
+		
+		while(!unsettled.isEmpty()) {
+			Station minStation = unsettled.poll();
+			minStation.setSettled(true);
+			
+			for(Edge edge : minStation.getEdges()) {
+				Station adjStation = edge.getDest();
+				long currMinutes = minStation.getMinutes() + edge.getMinute();
+				if(adjStation.getMinutes() > currMinutes) {
+					adjStation.setMinutes(currMinutes);
+					adjStation.setPrev(minStation);
+					
+					unsettled.remove(adjStation);
+					unsettled.add(adjStation);
+				}
+			}
+			count++;
+		}
+		
+		Station dest = Collections.min(destStations);
+		StringBuilder sb = new StringBuilder();
+		
+		Station curr = dest;
+		while(curr != null) {
+			Station prev = curr.getPrev();
+			if(prev != null && curr.getName().equals(prev.getName())) {
+				sb.insert(0, String.format("[%s] ", curr.getName()));
+				curr = prev.getPrev();
+			} else {
+				sb.insert(0, curr.getName() + " ");
+				curr = prev;
+			}
+		}
+		System.out.println(sb.toString().trim());
+		System.out.println(dest.getMinutes());
 	}
 	
-	public void print() {
-		for(Map.Entry<Station, LinkedList<Edge>> entry : hm.entrySet()) {
-		    Station key = entry.getKey();
-		    LinkedList<Edge> value = entry.getValue();
-		    
-		    System.out.println(" << " + key.getName() + " >> ");
-		    for(Edge edge : value) {
-		    	System.out.println(edge.getSource().getName() + " to " + edge.getDest().getName() + " = " + edge.getMinute() + "mins");
-		    }
+	private static void initNodes() {
+		for(Station st : idMap.values()) {
+			st.setMinutes(Long.MAX_VALUE);
+			st.setSettled(false);
+			st.setPrev(null);
 		}
 	}
 }
 
-class Station {
+class Station implements Comparable<Station> {
 	private String id;
 	private String name;
 	private String line;
+	private LinkedList<Edge> edges;
+	private long minutes;
+	private boolean settled;
+	private Station prev;
 	
 	public Station(String id, String name, String line) {
+		super();
 		this.id = id;
 		this.name = name;
 		this.line = line;
+		
+		this.edges = new LinkedList<>();
+		this.minutes = Long.MAX_VALUE;
+		this.settled = false;
+		this.prev = null;
 	}
 
 	public String getId() {
@@ -156,37 +181,65 @@ class Station {
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public String getLine() {
 		return line;
 	}
-	
+
 	public void setLine(String line) {
 		this.line = line;
+	}
+	
+	public LinkedList<Edge> getEdges() {
+		return edges;
+	}
+
+	public void setEdges(LinkedList<Edge> edges) {
+		this.edges = edges;
+	}
+
+	public long getMinutes() {
+		return minutes;
+	}
+
+	public void setMinutes(long minutes) {
+		this.minutes = minutes;
+	}
+
+	public boolean isSettled() {
+		return settled;
+	}
+
+	public void setSettled(boolean settled) {
+		this.settled = settled;
+	}
+
+	public Station getPrev() {
+		return prev;
+	}
+
+	public void setPrev(Station prev) {
+		this.prev = prev;
+	}
+
+	@Override
+	public int compareTo(Station st) {
+		return Long.compare(minutes, st.minutes);
 	}
 }
 
 class Edge {
-	private Station source;
 	private Station dest;
-	int minute;
-
-	public Edge(Station source, Station dest, int minute) {
-		this.source = source;
+	private long minute;
+	
+	public Edge(Station dest, long minute) {
+		super();
 		this.dest = dest;
 		this.minute = minute;
-	}	
-	
-	public Station getSource() {
-		return source;
-	}
-
-	public void setSource(Station source) {
-		this.source = source;
 	}
 
 	public Station getDest() {
@@ -197,11 +250,12 @@ class Edge {
 		this.dest = dest;
 	}
 
-	public int getMinute() {
+	public long getMinute() {
 		return minute;
 	}
 
-	public void setMinute(int minute) {
+	public void setMinute(long minute) {
 		this.minute = minute;
-	}
+	}	
+	
 }
